5ce1d4ab0e8a9c944b19696910a3faeebc229104
roboter5123 <73991535+roboter5123@users.noreply.github.com>
2024-05-28 14:58:13 +0200

    Update PlayerKilledListener.java

--- src/main/java/com/roboter5123/robogames/listener/PlayerKilledListener.java
+++ src/main/java/com/roboter5123/robogames/listener/PlayerKilledListener.java
@@ -6,6 +6,7 @@ import com.roboter5123.robogames.service.LanguageService;
 import com.roboter5123.robogames.service.PlayerService;
 import com.roboter5123.robogames.service.SchedulerService;
 import com.roboter5123.robogames.tasks.BroadCastIngameTask;
+import org.bukkit.event.Listener;
 
 public class PlayerKilledListener implements Listener {
 
